8c257055ac1bfed8746151b2123098181da33222
Alex Hoang Hai Nguyen <hoanghai92@hotmail.com>
2017-04-21 10:22:18 +0700

    init red dragon controller

---
+++ Assets/Scripts/Reddragonscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reddragonscript : MonoBehaviour {
+	public Sprite redDragon;
+	public Sprite whiteDragon;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<SpriteRenderer>().sprite = redDragon;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = whiteDragon;
+
+		}
+			
+		
+	}
+}
