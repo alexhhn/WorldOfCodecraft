31efffaed404a62a24144fccbde5936ea60d6f1b
Alex Hoang Hai Nguyen <hoanghai92@hotmail.com>
2017-04-21 11:19:45 +0700

    add freeze effect

--- Assets/Scripts/Reddragonscript.cs
+++ Assets/Scripts/Reddragonscript.cs
@@ -1,24 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Reddragonscript : MonoBehaviour {
 	public Sprite redDragon;
-	public Sprite whiteDragon;
+	public Sprite frozenRedDragon;
+	public Button yourButton;
 
 	// Use this for initialization
 	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
 		this.gameObject.GetComponent<SpriteRenderer>().sprite = redDragon;
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.F)) {
-			this.gameObject.GetComponent<SpriteRenderer>().sprite = whiteDragon;
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = frozenRedDragon;
 
 		}
-			
-		
 	}
+		
 }
